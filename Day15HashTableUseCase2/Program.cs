681cf67aed9dd6f42bc1b1f36c04185c7d85d414
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-06 14:18:41 +0530

    ok

---
+++ Day15HashTableUseCase2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace Day15HashTableUseCase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Hashtable ht = new Hashtable();
+
+                ht.Add("001", "To");
+                ht.Add("002", "Be");
+                ht.Add("003", "Or");
+                ht.Add("004", "Not");
+                ht.Add("005", "To");
+                ht.Add("006", "Be");
+
+                // Get a collection of the keys.
+                ICollection key = ht.Keys;
+
+                foreach (string k in key)
+                {
+                    Console.WriteLine(k + ": " + ht[k]);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+
